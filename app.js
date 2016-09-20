c4570c996043e459700fbd62077af5eef34b8f6f
kc910521 <kc910521@qq.com>
2016-09-20 10:25:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -30,35 +31,13 @@ app.engine('html', hbs.__express);
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+app.use(bodyParser.urlencoded());
 app.use('/', routes);
 app.use('/users', users);
+
 app.use(function(request, response, next) {
   console.log("In comes a " + request.method + " to " + request.url);
   next();
@@ -72,10 +51,27 @@ app.use(function(req, res, next) {
 
 // error handlers
 
-
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
 // production error handler
-
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 
 module.exports = app;
